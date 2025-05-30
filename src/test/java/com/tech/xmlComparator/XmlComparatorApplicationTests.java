180dd4643cc18d45a3cc1e773910d518ff20299b
DevDah1ya <109794920+DevDah1ya@users.noreply.github.com>
2025-05-30 11:04:54 +0530

    Setting Up Git Repo

---
+++ src/test/java/com/tech/xmlComparator/XmlComparatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.xmlComparator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlComparatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
